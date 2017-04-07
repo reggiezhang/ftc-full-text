0a93f4b12fba1c94a4ce47552d3c15474d790bc8
Reggie Zhang <reggie.zhang@sap.com>
2017-04-07 21:41:53 +0800

    commit by git-sync.sh

--- background.js
+++ background.js
@@ -1,6 +1,35 @@
-// http://stackoverflow.com/questions/21192345/chrome-extension-api-add-parameter-to-current-url
-
 var customParam = encodeURI('full=y');
+
+function match(url){  
+  var host = "null";  
+  if(typeof url == "undefined" || null == url)  
+  url = window.location.href;  
+  var regex = /.*\:\/\/([^\/]*).*/;  
+  var match = url.match(regex);  
+  if(typeof match != "undefined" && null != match)  
+  host = match[1];  
+  return host;  
+}  
+
+function entry(tabId, changeInfo, tab) {  
+    var url = tab.url;
+    if (changeInfo.status != "complete"
+      || match(url).toLowerCase() != "www.ftchinese.com"
+      || url.indexOf("ftchinese.com/story") == -1 
+      || url.indexOf("full=y") > 0) {
+        return; 
+    }
+    
+    var hashStart = (url.indexOf('#') === -1) ? url.length : url.indexOf('#');
+    var querySymbol = (url.indexOf('?') === -1) ? '?' : '&';
+    var newUrl = url.substring(0, hashStart) + querySymbol + customParam +
+                url.substring(hashStart);
+
+    chrome.tabs.update(tab.id, {url: newUrl});
+};  
+
+chrome.tabs.onUpdated.addListener(entry);  
+
 chrome.browserAction.onClicked.addListener(function (tab) {
   var url = tab.url;
   var hashStart = (url.indexOf('#') === -1) ? url.length : url.indexOf('#');

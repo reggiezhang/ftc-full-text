5683c92ccca0901fa676c96d15324d25ebedf0e5
Reggie Zhang <reggie.zhang@sap.com>
2017-04-08 17:41:22 +0800

    commit by git-sync.sh

--- background.js
+++ background.js
@@ -1,57 +1,52 @@
 function match(url) {
-  var host = "null";
-  if (typeof url == "undefined" || null == url)
-    url = window.location.href;
-  var regex = /.*\:\/\/([^\/]*).*/;
-  var match = url.match(regex);
-  if (typeof match != "undefined" && null != match)
-    host = match[1];
-  return host;
+    var host = "null";
+    if (typeof url == "undefined" || null == url)
+        url = window.location.href;
+    var regex = /.*\:\/\/([^\/]*).*/;
+    var match = url.match(regex);
+    if (typeof match != "undefined" && null != match)
+        host = match[1];
+    return host;
 }
 
 function reloadInFullText(tabId, url) {
-  var hashStart = (url.indexOf('#') === -1) ? url.length : url.indexOf('#');
-  var querySymbol = (url.indexOf('?') === -1) ? '?' : '&';
-  var newUrl = url.substring(0, hashStart) + querySymbol + encodeURI('full=y') +
-    url.substring(hashStart);
+    var hashStart = (url.indexOf('#') === -1) ? url.length : url.indexOf('#');
+    var querySymbol = (url.indexOf('?') === -1) ? '?' : '&';
+    var newUrl = url.substring(0, hashStart) + querySymbol + encodeURI('full=y') +
+        url.substring(hashStart);
 
-  chrome.tabs.update(tabId, { url: newUrl });
+    chrome.tabs.update(tabId, { url: newUrl });
+}
+
+function noNeedProceed(url) {
+    return match(url).toLowerCase() != "www.ftchinese.com"
+        || url.indexOf("ftchinese.com/story") == -1
+        || url.indexOf("full=y") > 0;
 }
 
 chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
-  var url = tab.url;
-  if (info.status != "complete"
-    || match(url).toLowerCase() != "www.ftchinese.com"
-    || url.indexOf("ftchinese.com/story") == -1
-    || url.indexOf("full=y") > 0) {
-    return;
-  }
-
-  chrome.tabs.executeScript(tab.id, {
-    file: "script.js"
-  }, function (results) {
-    if (results > 0) {
-      reloadInFullText(tab.id, tab.url);
+    if (info.status != "complete"
+        || noNeedProceed(tab.url)) {
+        return;
     }
-  });
-
+    chrome.tabs.executeScript(tab.id, {
+        file: "script.js"
+    }, function (results) {
+        if (results > 0) {
+            reloadInFullText(tab.id, tab.url);
+        }
+    });
 });
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-
-  var url = tab.url;
-  if (match(url).toLowerCase() != "www.ftchinese.com"
-    || url.indexOf("ftchinese.com/story") == -1
-    || url.indexOf("full=y") > 0) {
-    return;
-  }
-
-  chrome.tabs.executeScript(tab.id, {
-    file: "script.js"
-  }, function (results) {
-    if (results > 0) {
-      reloadInFullText(tab.id, tab.url);
+    if (noNeedProceed(tab.url)) {
+        return;
     }
-  });
-
+    chrome.tabs.executeScript(tab.id, {
+        file: "script.js"
+    }, function (results) {
+        if (results > 0) {
+            reloadInFullText(tab.id, tab.url);
+        }
+    });
 });

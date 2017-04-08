4cbf14844032f45ca2183cddb543d4e97f6155af
Reggie Zhang <reggie.zhang@sap.com>
2017-04-08 21:19:43 +0800

    support print mode

--- background.js
+++ background.js
@@ -9,43 +9,47 @@ function match(url) {
     return host;
 }
 
-function reloadInFullText(tabId, url) {
-    var hashStart = (url.indexOf('#') === -1) ? url.length : url.indexOf('#');
-    var querySymbol = (url.indexOf('?') === -1) ? '?' : '&';
-    var newUrl = url.substring(0, hashStart) + querySymbol + encodeURI('full=y') +
-        url.substring(hashStart);
-
-    chrome.tabs.update(tabId, { url: newUrl });
-}
-
 function notFtcStoryPage(url) {
     return match(url).toLowerCase() != "www.ftchinese.com"
-        || url.indexOf("ftchinese.com/story") == -1
-        || url.indexOf("full=y") > 0;
+        || url.indexOf("ftchinese.com/story") == -1;
+}
+
+function containFullParam(url) {
+    return url.indexOf("full=y") > 0;
+}
+
+function containPrintParam(url) {
+    return url.indexOf("print=y") > 0;
 }
 
 function stillLoading(status) {
     return status != "complete";
 }
+function reloadWithParam(tabId, url, param) {
+    var hashStart = (url.indexOf('#') === -1) ? url.length : url.indexOf('#');
+    var querySymbol = (url.indexOf('?') === -1) ? '?' : '&';
+    var newUrl = url.substring(0, hashStart) + querySymbol + encodeURI(param) +
+        url.substring(hashStart);
 
-function reloadStoryPageIfNeeded(tab) {
+    chrome.tabs.update(tabId, { url: newUrl });
+}
+function reloadStoryPageInFullText(tab) {
     chrome.tabs.executeScript(tab.id, {
         file: "script.js"
     }, function (results) {
         if (results > 0) {
-            reloadInFullText(tab.id, tab.url);
+            reloadWithParam(tab.id, tab.url, "full=y");
         }
     });
 }
-
 chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
-    if (stillLoading(info.status) || notFtcStoryPage(tab.url)) return;
+    if (stillLoading(info.status) || notFtcStoryPage(tab.url) || containFullParam(tab.url)) return;
     // reload ftc story page in full text mode after page loading completed
-    reloadStoryPageIfNeeded(tab);
+    reloadStoryPageInFullText(tab);
 });
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-    if (notFtcStoryPage(tab.url)) return;
+    if (notFtcStoryPage(tab.url) || containPrintParam(tab.url)) return;
     // reload ftc story page in full text mode
-    reloadStoryPageIfNeeded(tab);
+    reloadWithParam(tab.id, tab.url, "print=y");
 });

4a1c7bf3ca3f4d3f7fa95f724b63cfeb01d4e3ef
Reggie Zhang <reggie.zhang@sap.com>
2017-05-01 11:27:32 +0800

    commit by git-sync.sh

--- background.js
+++ background.js
@@ -1,3 +1,10 @@
+/*
+ * ftc-full-text
+ * Copyright (C) 2017 Reggie Zhang <reggy.zhang@gmail.com>
+ * Licensed under the terms of The GNU Lesser General Public License (LGPLv3):
+ * http://www.opensource.org/licenses/lgpl-3.0.html
+ * 
+ */
 chrome.tabs.onUpdated.addListener(function (tabId, info, tab) {
     if (loadCompleted(info.status) && isFtcStoryPage(tab.url) && notContainFullParam(tab.url)) {
         // reload ftc story page in full text mode after page loading completed
--- paginationCheck.js
+++ paginationCheck.js
@@ -1,3 +1,10 @@
+/*
+ * ftc-full-text
+ * Copyright (C) 2017 Reggie Zhang <reggy.zhang@gmail.com>
+ * Licensed under the terms of The GNU Lesser General Public License (LGPLv3):
+ * http://www.opensource.org/licenses/lgpl-3.0.html
+ * 
+ */
 var pagObj = document.getElementsByClassName('pagination-inner');
 var ret = pagObj.length;
 ret;
